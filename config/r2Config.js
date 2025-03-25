78a49a4a10754478a1936c347560725bc0ec945e
Pranjal Sharma <pranjal.ai@outlook.com>
2025-03-25 12:14:35 +0530

    Fix EAS errors

--- config/r2Config.js
+++ config/r2Config.js
@@ -1,7 +1,7 @@
 export const R2_CONFIG = {
-  accessKeyId: 'ACCESS_KEY_ID',
-  secretAccessKey: 'SECRECT_ACCESS_KEY',
-  endpoint: 'https://YOUR_ACCOUNT_ID.r2.cloudflarestorage.com',
+  accessKeyId: '90fb7b22ab6188f6533d89687db014a4',
+  secretAccessKey: 'f95bc846590a0a44456ec8a75312e5081a6bae8dd79198c055683371727f9d65',
+  endpoint: 'https://264f6d64d30e423773358f553ad62463.r2.cloudflarestorage.com',
   region: 'auto',
-  bucket: 'BUCKET_NAME'
+  bucket: 'nxt-cloud'
 };
